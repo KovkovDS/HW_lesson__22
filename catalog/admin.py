2888a6910ee47205229ea3a8862d13dd87279ba3
Dmitrii Kovkov <dmitrii.s.kovkov@outlook.com>
2024-12-22 23:56:38 +0300

    models Product and Category are registered in the admin area and the displays for them are configured

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Product
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name_c')
+    search_fields = ('name_c', 'description_c')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name_p', 'price_by', 'category')
+    list_filter = ('category',)
+    search_fields = ('name_p', 'description_p')
