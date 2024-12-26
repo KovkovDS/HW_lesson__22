dbf8b99d141f69f0ceda5a49a8d5ac21eebcf404
Dmitrii Kovkov <dmitrii.s.kovkov@outlook.com>
2024-12-26 23:36:46 +0300

    added a template for adding a new product and functionality for page-by-page display of a list of products on the main page

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from .views import home, contacts, product_detail
+from .views import home, contacts, product_detail, adding_product
 
 
 app_name = CatalogConfig.name
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
     path('product/<int:product_id>/', product_detail, name='product'),
+    path('adding_product/', adding_product, name='adding_product'),
 ]
