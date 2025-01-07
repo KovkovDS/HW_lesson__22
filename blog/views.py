36c640344c6bee2adab3692c3f9104172fc7c10a
Dmitrii Kovkov <dmitrii.s.kovkov@outlook.com>
2025-01-08 02:41:43 +0300

    added functionality for increasing the view count, filtering for displaying published articles, and redirection functionality after editing an article to the page of the same article.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from blog.models import BlogArticle
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, TemplateView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 
 class BlogArticlesListView(ListView):
@@ -10,17 +10,21 @@ class BlogArticlesListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'articles'
 
-
-# class Contacts(ListView):
-#     model = Contact
-#     template_name = 'contacts.html'
-#     context_object_name = 'contacts'
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(published=True)
 
 
 class BlogArticleDetailView(DetailView):
     model = BlogArticle
     template_name = 'blog/article.html'
     context_object_name = 'article'
+
+    def get_object(self, **kwargs):
+        self.object = super().get_object(**kwargs)
+        self.object.number_views += 1
+        self.object.save()
+        return self.object
 
 
 class AddedBlogArticle(TemplateView):
@@ -37,5 +41,20 @@ class BlogArticleCreateView(CreateView):
 
     def get_success_url(self, **kwargs):
         success_url = super().get_success_url()
-        added_article = self.object
-        return "{0}?param={1}".format(success_url, added_article)
+        return "{0}?param={1}".format(success_url, self.kwargs.get('pk'))
+
+
+class BlogArticleUpdateView(UpdateView):
+    model = BlogArticle
+    fields = ['title', 'content', 'preview', 'published']
+    template_name = 'blog/editing_article.html'
+    success_url = reverse_lazy('blog:home')
+
+    def get_success_url(self, **kwargs):
+        return reverse('blog:article', args=[self.kwargs.get('pk')])
+
+
+class BlogArticleDeleteView(DeleteView):
+    model = BlogArticle
+    template_name = 'blog/articles_confirm_delete.html'
+    success_url = reverse_lazy('blog:home')
